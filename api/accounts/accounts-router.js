def6dbbe3a7d1044c44c36a7a6cf1dd9972c4efb
Sterling Sanders <sterlingsanders723@gmail.com>
2023-11-30 14:53:06 -0800

    done

--- api/accounts/accounts-router.js
+++ api/accounts/accounts-router.js
@@ -1,27 +1,63 @@
 const router = require('express').Router()
+const Account = require('./accounts-model');
+const md = require('./accounts-middleware');
+router.get('/', async (req, res, next) => {
+  // DO YOUR MAGIC
+  try {
+    const data = await Account.getAll();
+    res.json(data)
+  } catch (err) {
+    next(err)
+  }
+})
 
-router.get('/', (req, res, next) => {
+router.get('/:id', md.checkAccountId, async (req, res, next) => {
+  res.json(req.account)
   // DO YOUR MAGIC
 })
 
-router.get('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+router.post('/', md.checkAccountPayload,
+  md.checkAccountNameUnique,
+  async (req, res, next) => {
+    // DO YOUR MAGIC
+    try {
+      const newAccount = await Account.create({ name: req.body.name.trim(), budget: req.body.budget, });
+      res.status(201).json(newAccount)
+    } catch (err) {
+      next(err);
+    }
+  })
 
-router.post('/', (req, res, next) => {
-  // DO YOUR MAGIC
-})
+router.put('/:id',
+  md.checkAccountId,
+  md.checkAccountPayload,
+  async (req, res, next) => {
+    // DO YOUR MAGIC
+    const updated = await Account.updateById(req.params.id, req.body)
+    res.json(updated)
+    try {
+      res.json('updated')
+    } catch (err) {
+      next(err)
+    }
+  });
 
-router.put('/:id', (req, res, next) => {
+router.delete('/:id', md.checkAccountId, async (req, res, next) => {
   // DO YOUR MAGIC
-});
-
-router.delete('/:id', (req, res, next) => {
-  // DO YOUR MAGIC
+  try {
+    await Account.deleteById(req.params.id);
+    res.json(req.account);
+  }
+  catch (err) {
+    next(err)
+  }
 })
 
 router.use((err, req, res, next) => { // eslint-disable-line
   // DO YOUR MAGIC
+  res.status(err.status || 500).json({
+    message: err.message
+  })
 })
 
 module.exports = router;
--- queries.sql
+++ queries.sql
@@ -1,13 +1,13 @@
 -- Database Queries
 
 -- Find all customers with postal code 1010
-
+SELECT * FROM [Customers] where postalcode=1010;
 -- Find the phone number for the supplier with the id 11
-
+SELECT phone FROM [Suppliers] where supplierid = 11;
 -- List first 10 orders placed, sorted descending by the order date
-
+SELECT * FROM [Orders] order by orderid, orderdate desc limit 10;
 -- Find all customers that live in London, Madrid, or Brazil
-
+SELECT * FROM [Customers] where city='Madrid'or city='London'or city='Brazil';
 -- Add a customer record for "The Shire", the contact name is "Bilbo Baggins" the address is -"1 Hobbit-Hole" in "Bag End", postal code "111" and the country is "Middle Earth"
 
 -- Update Bilbo Baggins record so that the postal code changes to "11122"
